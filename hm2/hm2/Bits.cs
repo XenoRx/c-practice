461cac76346833d548042b255fc4e75ff0866842
disappear <disappear123@mail.ru>
2023-10-23 13:57:17 +0300

    hm2 done

---
+++ hm2/hm2/Bits.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hm2
+{
+    public class Bits
+    {
+        public readonly byte[] Data;
+
+        public Bits(long value)
+        {
+            Data = BitConverter.GetBytes(value);
+        }
+
+        public Bits(int value)
+        {
+            Data = BitConverter.GetBytes(value);
+        }
+
+        public Bits(byte value)
+        {
+            Data = new byte[1];
+            Data[0] = value;
+        }
+
+        public static implicit operator Bits(long value)
+        {
+            return new Bits(value);
+        }
+
+        public static implicit operator Bits(int value)
+        {
+            return new Bits(value);
+        }
+
+        public static implicit operator Bits(byte value)
+        {
+            return new Bits(value);
+        }
+    }
+}
---
+++ hm2/hm2/Main.cs
@@ -0,0 +1,29 @@
+﻿using hm2;
+using Xunit;
+
+// Создаем объект Bits из long
+long value = 1234567890;
+Bits bits = value;
+
+// Проверяем, что массив байтов объекта Bits содержит ожидаемые значения
+byte[] bytes = bits.Data;
+bytes[0] = 0x00;
+Assert.Equal(bytes[0], 0x00);
+
+// Создаем объект Bits из int
+int value2 = 12345;
+Bits bits2 = value2;
+
+// Проверяем, что массив байтов объекта Bits содержит ожидаемые значения
+bytes = bits2.Data;
+bytes[0] = 0x00;
+Assert.Equal(bytes[0], 0x00);
+
+// Создаем объект Bits из byte
+byte value3 = 127;
+Bits bits3 = value3;
+
+// Проверяем, что массив байтов объекта Bits содержит ожидаемые значения
+bytes = bits3.Data;
+bytes[0] = 0x7f;
+Assert.Equal(bytes[0], 0x7f);

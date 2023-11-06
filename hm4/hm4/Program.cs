eb283d2d9b58cb2cc04bf95536fd4ff468831535
disappear <disappear123@mail.ru>
2023-11-06 13:48:36 +0300

    hm4 done

---
+++ hm4/hm4/Program.cs
@@ -0,0 +1,37 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] array = {1, 2, 3, 4, 5 ,6 ,6 ,7 ,7 ,8 ,9};
+        int target = 15;
+        FindTriplet(array, target);
+
+        static bool FindTriplet(int[] arr, int targetSum)
+        {
+            for (int i = 0; i < arr.Length - 2; i++)
+            {
+                int left = i + 1;
+                int right = arr.Length - 1;
+
+                while (left < right)
+                {
+                    int currentSum = arr[i] + arr[left] + arr[right];
+
+                    if (currentSum == targetSum)
+                    {
+                        Console.WriteLine(arr[i] + " " + arr[left] + " " + arr[right]);
+                        return true;
+                    }
+
+                    else if (currentSum < targetSum)
+                        left++;
+
+                    else
+                        right--;
+                }
+            }
+
+            return false;
+        }
+    }
+}

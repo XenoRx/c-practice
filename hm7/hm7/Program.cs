192f877d0bce194e51b065a186a38ebefd601324
disappear <disappear123@mail.ru>
2023-11-23 13:38:50 +0300

    hm7 done

---
+++ hm7/hm7/Program.cs
@@ -0,0 +1,16 @@
+﻿using hm7;
+
+Console.WriteLine("Введите число: ") ;
+int userNumber = int.Parse(Console.ReadLine());
+var test = new TestClass()
+{
+    I = userNumber
+};
+var data = test.ObjectToString(); // customFieldName = 10;
+
+test.StringProperty =  "123";
+
+var newObj = new TestClass();
+newObj.StringToObject(data);
+Console.WriteLine();
+Console.WriteLine($"Вывод: {newObj.I}");
---
+++ hm7/hm7/TestClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hm7
+{
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+    public class CustomNameAttribute : Attribute
+    {
+        public string Name { get; set; }
+
+        public CustomNameAttribute(string name)
+        {
+            Name = name;
+        }
+    }
+
+    internal class TestClass
+    {
+        [CustomName("CustomFieldName")]
+        public int I = 10;
+
+        public string StringProperty = "Some string";
+    }
+}
